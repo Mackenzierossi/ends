c787456a7f074f3836bddcf209cf7ad4851b0778
Mackenzie Rossi <mackenzierossi@mackenzies-macbook.local>
2018-01-23 14:42:42 -0800

    Initial Commit

---
+++ ends/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ends
+//
+//  Created by Mackenzie Rossi on 1/23/18.
+//  Copyright © 2018 Mackenzie Rossi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
